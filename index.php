9eb52d6133a0e8762a83ac8855fb0b59d3c2fd8e
Andreas Eriksson <andene@gmail.com>
2014-03-28 11:29:49 +0100

    Removed some unnecessary imports

--- index.php
+++ index.php
@@ -4,14 +4,12 @@ require "vendor/autoload.php";
 
 use \Oak\Route\Route as Route;
 use \Oak\Route\Routes as Routes;
-use \Oak\Header\Request as Request;
-use \Oak\Header\Redirect as Redirect;
 use \Oak\App\App as App;
 
 $routes = new Routes();
 $routes->addRoute(new Route('home', 'index', 'index'));
 $routes->addRoute(new Route('index/information', 'index', 'information'));
-$routes->addRoute(new Route('kontakt', 'index', 'contact'));
+$routes->addRoute(new Route('kontakt', 'index', 'hej'));
 $routes->addRoute(new Route('kontakt/{office}', 'index', 'office'));
 $routes->addRoute(new Route('user/{username}/{id}', 'index', 'about'));
 $routes->addRoute(new Route('user/{username}/{id}/del', 'index', 'profile'));
