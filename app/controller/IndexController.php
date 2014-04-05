fb5e81a531c49b9b1578139d23c7482f151c29a5
Andreas Eriksson <andene@gmail.com>
2014-04-05 14:09:09 +0200

    Revert "removed controller folder with lowercase c"
    This reverts commit 9e90419b7c0725a769763c885b570b36755b5a0b.

---
+++ app/controller/IndexController.php
@@ -0,0 +1,41 @@
+<?hh namespace App\Controller;
+
+
+class IndexController extends \Oak\Controller\BaseController {
+
+    public string $title;
+
+    public function __construct():void {
+        parent::__construct();
+
+        $this->title = "Oak Framework 0.1.0";
+
+    }
+
+    public function index():\Oak\View\View {
+
+
+        $view = new \Oak\View\View('index.index');
+        $view->with('title', $this->title)->with('headline', 'Min rubrik');
+
+        return $view;
+
+    }
+
+    public function office($officeName): string {
+
+        return "Welcome to " . $officeName;
+    }
+
+    public function about($username, $id) :\Oak\View\View {
+
+        $view = new \Oak\View\View('index.user');
+
+        $headline = "Välkommen " . $username . " (".$id.")";
+
+        $view->with('title', $this->title)->with('headline', $headline);
+        return $view;
+    }
+
+
+}
